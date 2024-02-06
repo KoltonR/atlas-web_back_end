e01f66bdd26cd127ceccc7ef7cb629966e2878d0
Kolton Rhodes <gamerlife467@gmail.com>
2024-02-05 19:23:40 -0600

    redo

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,11 +1,11 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-      if (typeof name !== 'string') {
-    throw new TypeError('Name must be a string');
-      }
-      if (typeof length !== 'number') {
-    throw new TypeError('length must be a number');
-      }
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    if (typeof length !== 'number') {
+      throw new TypeError('length must be a number');
+    }
       if (!Array.isArray(students)) {
     throw new TypeError('students must be an array');
       }
