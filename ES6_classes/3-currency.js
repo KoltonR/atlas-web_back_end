a0e3bafe98dc4a8ecafb95d69aa4c8cd18d62ecd
Kolton Rhodes <gamerlife467@gmail.com>
2024-02-05 19:55:18 -0600

    redo

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -1,30 +1,30 @@
 export default class Currency {
-    constructor(code, name) {
-      this._code = code;
-      this._name = name;
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(newCode) {
+    if (typeof newCode !== 'string') {
+      throw new TypeError('Code must be a string.');
+    } else {
+      this._code = newCode;
     }
-  
-    displayFullCurrency() {
-      return `${this._name} (${this._code})`;
+  }
+
+  get name() {
+    return this._name;
     }
-  
-    get code() {
-      return this._code;
-    }
-  
-    set code(newCode) {
-      if (typeof newCode !== 'string') {
-        throw new TypeError('Code must be a string.');
-      } else {
-        this._code = newCode;
-      }
-    }
-  
-    get name() {
-      return this._name;
-    }
-  
-    set name(newName) {
+
+  set name(newName) {
       if (typeof newName !== 'string') {
         throw new TypeError('Name must be a string.');
       } else {
